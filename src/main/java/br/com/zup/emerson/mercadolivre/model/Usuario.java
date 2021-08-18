00da1305599f3be67870706f5cbf1da36594289b
EmersonReisCosta <emerson.costa@zup.com.br>
2021-08-18 09:44:41 -0300

    Mapeado a entidade Usuario

--- src/main/java/br/com/zup/emerson/mercadolivre/model/Usuario.java
+++ src/main/java/br/com/zup/emerson/mercadolivre/model/Usuario.java
@@ -1,4 +1,31 @@
 package br.com.zup.emerson.mercadolivre.model;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Entity
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String login;
+    @NotBlank
+    @Size(min = 6)
+    private String senha;
+    @CreationTimestamp
+    private LocalDate horaCadastro;
+
+    @Deprecated
+    public Usuario() {
+    }
 }
