fa796688f43ffffdf22e524b36bf9531834fbc9a
EmersonReisCosta <emerson.costa@zup.com.br>
2021-08-20 14:48:03 -0300

    Finalizado cadastro de Categoria

--- src/main/java/br/com/zup/emerson/mercadolivre/controller/CategoriaController.java
+++ src/main/java/br/com/zup/emerson/mercadolivre/controller/CategoriaController.java
@@ -2,6 +2,7 @@ package br.com.zup.emerson.mercadolivre.controller;
 
 import br.com.zup.emerson.mercadolivre.controller.dto.request.CategoriaRequest;
 import br.com.zup.emerson.mercadolivre.model.Categoria;
+import br.com.zup.emerson.mercadolivre.repository.CategoriaRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +14,16 @@ import javax.validation.Valid;
 @RequestMapping("/categorias")
 public class CategoriaController {
 
+    CategoriaRepository categoriaRepository;
+
+    public CategoriaController(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
     @PostMapping
     public Categoria cadastraCategoria(@RequestBody @Valid CategoriaRequest categoriaRequest){
-        Categoria categoria = categoriaRequest.toModel();
+        Categoria categoria = categoriaRequest.toModel(categoriaRepository);
+        categoriaRepository.save(categoria);
         return categoria;
     }
 }
--- src/main/java/br/com/zup/emerson/mercadolivre/controller/dto/request/CategoriaRequest.java
+++ src/main/java/br/com/zup/emerson/mercadolivre/controller/dto/request/CategoriaRequest.java
@@ -1,26 +1,45 @@
 package br.com.zup.emerson.mercadolivre.controller.dto.request;
 
+
 import br.com.zup.emerson.mercadolivre.model.Categoria;
+import br.com.zup.emerson.mercadolivre.repository.CategoriaRepository;
+import br.com.zup.emerson.mercadolivre.validation.UniqueValue;
+
+import javax.validation.constraints.NotBlank;
 
 public class CategoriaRequest {
 
+    @NotBlank
+    @UniqueValue(domainClass = Categoria.class, fildName = "nomeCategoria")
     private String nomeCategoria;
-    private Categoria categoria;
+    private Long categoriaId;
 
-    public CategoriaRequest(String nomeCategoria, Categoria categoria) {
+    @Deprecated
+    public CategoriaRequest() {
+    }
+
+
+    public CategoriaRequest(String nomeCategoria, Long categoria_id) {
         this.nomeCategoria = nomeCategoria;
-        this.categoria = categoria;
+        this.categoriaId = categoriaId;
     }
 
     public String getNomeCategoria() {
         return nomeCategoria;
     }
 
-    public Categoria getCategoria() {
-        return categoria;
+    public Long getCategoriaId() {
+        return categoriaId;
     }
+
     // Categoria request deve receber o nome e a chave estrangeira de categoria.
-    public Categoria toModel(){
-        return new Categoria(this.nomeCategoria, this.categoria);
+    public Categoria toModel(CategoriaRepository categoriaRepository){
+        if (categoriaId != null){
+            Categoria categoriaEncontrada = categoriaRepository.findById(categoriaId).get();
+            return new Categoria(nomeCategoria, categoriaEncontrada);
+        }
+
+        return new Categoria(nomeCategoria, null);
+
     }
 }
