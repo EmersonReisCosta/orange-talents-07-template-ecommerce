3e27728a4810460642f2baecb1d80349beec2d74
EmersonReisCosta <emerson.costa@zup.com.br>
2021-08-20 09:16:19 -0300

    Criado CategoriaRequest

---
+++ src/main/java/br/com/zup/emerson/mercadolivre/controller/dto/request/CategoriaRequest.java
@@ -0,0 +1,22 @@
+package br.com.zup.emerson.mercadolivre.controller.dto.request;
+
+import br.com.zup.emerson.mercadolivre.model.Categoria;
+
+public class CategoriaRequest {
+
+    private String nomeCategoria;
+    private Categoria categoria;
+
+    public CategoriaRequest(String nomeCategoria, Categoria categoria) {
+        this.nomeCategoria = nomeCategoria;
+        this.categoria = categoria;
+    }
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+}
