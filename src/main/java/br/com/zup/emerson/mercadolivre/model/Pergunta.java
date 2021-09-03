28eb033abeccdba346e658a7100911e83f536843
EmersonReisCosta <emerson.costa@zup.com.br>
2021-09-03 09:38:13 -0300

    Entidade Pergunta criada

---
+++ src/main/java/br/com/zup/emerson/mercadolivre/model/Pergunta.java
@@ -0,0 +1,51 @@
+package br.com.zup.emerson.mercadolivre.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+public class Pergunta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @NotBlank
+    private String titulo;
+    @CreationTimestamp
+    private LocalDateTime instanteCriacao;
+    @NotNull
+    @ManyToOne
+    private Usuario usuarioLogado;
+    @NotNull
+    @ManyToOne
+    private Produto produto;
+
+    public Pergunta(String titulo, Usuario usuarioLogado, Produto produto) {
+        this.titulo = titulo;
+        this.usuarioLogado = usuarioLogado;
+        this.produto = produto;
+    }
+
+    public Pergunta() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pergunta pergunta = (Pergunta) o;
+        return Objects.equals(Id, pergunta.Id) && Objects.equals(titulo, pergunta.titulo) && Objects.equals(instanteCriacao, pergunta.instanteCriacao) && Objects.equals(usuarioLogado, pergunta.usuarioLogado) && Objects.equals(produto, pergunta.produto);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(titulo, usuarioLogado, produto);
+    }
+
+
+}
